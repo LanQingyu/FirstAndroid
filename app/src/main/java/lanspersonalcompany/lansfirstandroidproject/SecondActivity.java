206fe6bf3f9baf2fe3c941625537e9845374e8e7
apple <apple@appledeiMac.local>
2018-04-04 10:04:15 +0800

    no message

--- app/src/main/java/lanspersonalcompany/lansfirstandroidproject/SecondActivity.java
+++ app/src/main/java/lanspersonalcompany/lansfirstandroidproject/SecondActivity.java
@@ -1,24 +1,23 @@
 package lanspersonalcompany.lansfirstandroidproject;
 
-import android.content.Intent;
+/**
+ * Created by apple on 2018/1/12.
+ */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.button10);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_second);
+        Button button1 = findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
     }
